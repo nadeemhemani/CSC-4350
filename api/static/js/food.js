b68915c8dd6b02ea1ec9fcbcacc9cc98053c3a8e
Samantha Wolownik <55200697+samanthawolownik@users.noreply.github.com>
2021-04-24 03:24:44 -0400

    link fixes

--- api/static/js/food.js
+++ api/static/js/food.js
@@ -1,4 +1,12 @@
 const form = document.querySelector('#FoodList')
 
+id : sessionStorage.getItemI('storeid')
+ const getFoods = () => {
+    fetch('/foods/' + id)
+    .then(response => response.json())
+    .then(foods => console.log(foods))
+ }
+
+
 
 
